e34a2bdf3b66762dcd5b0d70670503a15fd798bf
skfro266 <skfro266@naver.com>
2018-11-15 16:48:45 +0900

    20181115

--- public/js/main.js
+++ public/js/main.js
@@ -22,10 +22,13 @@ $(".login button").on("click", function () {
     // auth.signInWithRedirect(googleAuth);
 });
 auth.onAuthStateChanged(function (result) {
-    if (result) init();
-    else {
+    if (result) {
+        user = result;
+        init();
+    } else {
         $(".login").show();
         $(".wrap").hide();
+        $(".lists").empty();
     }
 });
 
@@ -37,7 +40,53 @@ $(".logout").click(function () {
 function init() {
     $(".login").hide();
     $(".wrap").show();
+    $(".lists").empty();
+    ref = db.ref("root/memos/");
+    ref.on("child_added", onAdd);
+    ref.on("child_removed", onRev);
 }
+
+function onAdd(data) {
+    var id = data.key;
+    var val = data.val();
+    var html = '';
+    html += '<li id="' + id + '">';
+    html += '<h4>' + val.content + '</h4>';
+    html += '<h5>' + val.email + '</h5>';
+    html += '<button onclick="revData(this)"><i class="fa fa-trash"></i></button>';
+    html += '</li>';
+    $(".lists").prepend(html);
+}
+
+function onRev(data) {
+    $("#" + data.key).remove();
+}
+$("#bt_wr").click(function () {
+    var content = $("#content").val();
+    if (content == "") {
+        alert("내용을 입력하세요.");
+        $("#content").focus();
+    } else {
+        ref = db.ref("root/memos/");
+        ref.push({
+            content: content,
+            wdate: new Date().getTime(),
+            email: user.email
+        }).key
+        $("#content").val("");
+    }
+});
+
+function revData(obj) {
+    var id = $(obj).parent().attr("id");
+    if ($(obj).parent().find("h5").html() == user.email) {
+        ref = db.ref("root/memos/" + id);
+        ref.remove();
+    } 
+    else {
+        alert("타인의 글은 삭제할 수 없습니다.")
+    }
+};
 // $(".bts").click(function(){
 //     $(location).attr('href','../main.html');
 // });
